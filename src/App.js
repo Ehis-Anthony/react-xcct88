6a2ecfd17adc5a45c199433e87a3a67b702a8b89
Ehis-Anthony <speaktoehis77@gmail.com>
2021-05-24 08:28:44 +0100

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,32 +3,12 @@ import './style.css';
 
 import React, {Component} from 'react';
 import Table from "./Table";
+import Form from "./Form";
 
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-      {
-        name: 'Ehis',
-        job: 'Senior Frontend Engineer',
-      },
-    ],
+    characters: [],
   }
 
   removeCharacter = (index) => {
@@ -39,13 +19,20 @@ class App extends Component {
       return i !== index
     }),
   })
-}
+  }
+  
+  //function for submitting of data and updating the parent state
+  handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
 
   render() {
     const { characters } = this.state   
     return (
       <div className="table table-striped">
         <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+        <Form handleSubmit={this.handleSubmit}/>
           
       </div>
     )
